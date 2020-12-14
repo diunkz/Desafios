e46f140ec13410ca6ad8620edc8b88c457c44b4e
Junkes Junior <junkesjunior@live.com>
2020-12-14 17:27:16 -0400

    q3

--- fic/Aplicativo.java
+++ fic/Aplicativo.java
@@ -1,5 +1,6 @@
 public class Aplicativo {
     public static void main(String[] args) {
+
         System.out.println("**********       ***         *        *");
         System.out.println("*        *     *     *      ***      * *");
         System.out.println("*        *     *     *     *****    *   *");
